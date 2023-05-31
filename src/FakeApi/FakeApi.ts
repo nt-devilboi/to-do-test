1d3b14fd06ab08d76c3b7526041d002bec36e19c
Devilboi99 <turnickii.n@gmail.com>
2023-05-31 15:00:30 +0500

    изменил, входные данные

--- src/FakeApi/FakeApi.ts
+++ src/FakeApi/FakeApi.ts
@@ -4,12 +4,42 @@ import {Task} from "../Pages/Todo/Todo";
 export class FakeApi {
     static GetDataTodo(): Task[] {
         return [
-            {title: "C#", description: "изучать алгосы!!", isComplete: false,
-                subtasks: [{title: "задачи по regexp 1 час", isComplete: false}]},
-            {title: "Typesctript", description: "изучать mobx", isComplete: false,
+            {
+                id: 1,
+                title: "C#",
+                description: "изучать алгосы!!",
+                isOpen: false,
+                subtasks: [
+                    {
+                        id: 2,
+                        title: "задачи по regexp 1 час",
+                        isOpen: false,
+                        description: "bla bla bla",
+                        subtasks: [
+                            {
+                                id: 4,
+                                title: "Деревья",
+                                description: "сложное ааа",
+                                isOpen: false,
+                                subtasks: []
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                id: 3,
+                title: "Typesctript",
+                description: "изучать mobx",
+                isOpen: false,
                 subtasks: []}, // дописать
-            {title: "Прокт", description: "исправлять баги", isComplete: false,
-                subtasks: []}
+            {
+                id: 4,
+                title: "Прокт",
+                description: "исправлять баги",
+                isOpen: false,
+                subtasks: []
+            }
         ]
     }
 }
